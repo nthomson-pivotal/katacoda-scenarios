f84bb1dd7eb3bbb5e62e9af9038f0e0ec0c404ab
Niall Thomson <nthomson@pivotal.io>
2019-04-21 15:28:41 -0700

    Trying single line command

--- cloudfoundry-intro/foreground.sh
+++ cloudfoundry-intro/foreground.sh
@@ -1,6 +1,6 @@
 echo "Installing CloudFoundry CLI..."
 
-curl -q 'https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.43.0/cf-cli_6.43.0_linux_x86-64.tgz' -o cf.tgz
-tar zxf cf.tgz
-mv cf /usr/bin/cf
+curl -q 'https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.43.0/cf-cli_6.43.0_linux_x86-64.tgz' -o cf.tgz && \
+tar zxf cf.tgz && \
+mv cf /usr/bin/cf && \
 rm LICENSE NOTICE cf.tgz
