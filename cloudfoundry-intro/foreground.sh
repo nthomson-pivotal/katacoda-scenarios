e5fd404c23e219f52ffae342ba0f605bef4528c0
Niall Thomson <nthomson@pivotal.io>
2019-04-21 15:33:32 -0700

    Reworking scripts

--- cloudfoundry-intro/foreground.sh
+++ cloudfoundry-intro/foreground.sh
@@ -1,6 +1,1 @@
-echo "Installing CloudFoundry CLI..."
-
-curl -q 'https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.43.0/cf-cli_6.43.0_linux_x86-64.tgz' -o cf.tgz && \
-tar zxf cf.tgz && \
-mv cf /usr/bin/cf && \
-rm LICENSE NOTICE cf.tgz
+echo "Installing CloudFoundry CLI..."; while [ ! -f /usr/bin/cf ] ; do sleep 2; done;
