bf79b1ced21312998189b5e3c995d1cb0a702a44
Niall Thomson <nthomson@pivotal.io>
2019-04-21 16:32:25 -0700

    Install git in init script

--- cloudfoundry-intro/background.sh
+++ cloudfoundry-intro/background.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt update && apt get install -y git
+
 git clone https://github.com/cloudfoundry-samples/spring-music
 
 curl -q 'https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.43.0/cf-cli_6.43.0_linux_x86-64.tgz' -o cf.tgz && \
